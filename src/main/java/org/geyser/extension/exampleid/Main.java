c954b117a6dd59e3b51ff07b5038b34707fc1c5a
SonnenPingu <126945262+SonnenPingu@users.noreply.github.com>
2024-08-25 12:34:35 +0200

    Update Main.java

--- src/main/java/org/geyser/extension/exampleid/Main.java
+++ src/main/java/org/geyser/extension/exampleid/Main.java
@@ -3,17 +3,29 @@ package org.geyser.extension.exampleid;
 import org.geysermc.geyser.api.event.Subscribe;
 import org.geysermc.geyser.api.event.lifecycle.GeyserPostInitializeEvent;
 import org.geysermc.geyser.api.extension.Extension;
+import org.geysermc.geyser.api.event.manager.EventManager;
+import java.util.logging.Logger;
 
 public class Main extends Extension {
 
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
     @Override
     public void onInitialize() {
-        // Registriere die RegisterCustomSkull-Klasse
-        this.getEventBus().register(new RegisterCustomSkull());
+        // Ereignis-Manager erhalten
+        EventManager eventManager = this.getEventManager();
+        
+        // Ereignis registrieren
+        if (eventManager != null) {
+            eventManager.register(this, new Skull());
+            LOGGER.info("Skull wurde erfolgreich registriert.");
+        } else {
+            LOGGER.warning("EventManager konnte nicht initialisiert werden.");
+        }
     }
 
     @Subscribe
     public void onPostInitialize(GeyserPostInitializeEvent event) {
-        logger().info("Geyser Extension für benutzerdefinierte Skulls ist aktiv.");
+        LOGGER.info("Geyser Extension für benutzerdefinierte Skulls ist aktiv.");
     }
 }
